fab9d67d26b92a520f79d41c5c720d31562b54d7
tosiara <tosiara@users.noreply.github.com>
2017-04-06 14:58:33 +0300

    Added version string template {ver}

--- motion.c
+++ motion.c
@@ -3364,6 +3364,11 @@ static void mystrftime_long (const struct context *cnt,
         sprintf(out, "%*llu", width, cnt->database_event_id);
         return;
     }
+    if (SPECIFIERWORD("ver")) {
+        sprintf(out, "%*s", width, VERSION);
+        return;
+    }
+
 
     // Not a valid modifier keyword. Log the error and ignore.
     MOTION_LOG(ERR, TYPE_ALL, NO_ERRNO,
