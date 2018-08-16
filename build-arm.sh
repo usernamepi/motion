627de497104a6328fca1b668cb2a171a5ffe21a1
tosiara <tosiara@users.noreply.github.com>
2018-08-16 12:49:32 +0300

    Added install and autoreconf commands to build-arm.sh

--- build-arm.sh
+++ build-arm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install autoconf automake build-essential pkgconf libtool libzip-dev libjpeg-dev git libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavdevice-dev libwebp-dev libmicrohttpd-dev
+
+autoreconf -fiv
 CFLAGS="-O3 -std=gnu99 -march=native -mtune=native -mfloat-abi=hard -mfpu=neon" ./configure
 make -j3
 
