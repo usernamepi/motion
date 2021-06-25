2646749863e0ab0394773257e0d5d4921c9fc502
tosiara <tosiara@users.noreply.github.com>
2021-06-25 15:06:30 +0300

    Added silent install and some logging to build-arm.sh

--- build-arm.sh
+++ build-arm.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo apt-get install autoconf automake build-essential pkgconf libtool libzip-dev libjpeg-dev git libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavdevice-dev libwebp-dev libmicrohttpd-dev autopoint
+echo "Install motion compile dependencies"
+sudo apt-get -y install autoconf automake build-essential pkgconf libtool libzip-dev libjpeg-dev git libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavdevice-dev libwebp-dev libmicrohttpd-dev autopoint
 
+echo "autoreconf -fiv"
 autoreconf -fiv
 
 arch=`dpkg --print-architecture`
 if [ "$ARCH" == "arm" ] || [ "$arch" == "armhf" ]
 then
+	echo "Arch: $ARCH"
 	CFLAGS="-O3 -std=gnu99 -march=native -mtune=native -mfloat-abi=hard -mfpu=neon" ./configure
 	#CFLAGS="-O3 -std=gnu99 -march=armv7 -mtune=cortex-a9 -mfloat-abi=hard -mfpu=neon" ./configure
 else
+	echo "Arch: $ARCH"
 	CFLAGS="-O3 -std=gnu99 -march=native" ./configure
 fi
 
